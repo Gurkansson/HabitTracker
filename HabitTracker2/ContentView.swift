f60d7d8cc84cb83f4b99fd9193d716449923804c
Gurkansson <fil.gustavsson@gmail.com>
2024-05-08 08:20:49 +0200

    Initial Commit

---
+++ HabitTracker2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HabitTracker2
+//
+//  Created by Frida on 2024-05-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HabitTracker2/HabitTracker2App.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTracker2App.swift
+//  HabitTracker2
+//
+//  Created by Frida on 2024-05-08.
+//
+
+import SwiftUI
+
+@main
+struct HabitTracker2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
